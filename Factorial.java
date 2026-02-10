573dc1bcc4fcf9be9c7227c144a960f1aa097777
Palie <louis_palie@ens.univ-artois.fr>
2026-02-10 12:51:27 +0100

    Perturbations

--- Factorial.java
+++ Factorial.java
@@ -7,7 +7,7 @@ public class Factorial {
         if (n == 0 || n == 1) {
             return 1;
         }
-        return n * factorial(n - 1);
+        return n + factorial(n - 1);
     }
 
     public static void main(String[] args) {
